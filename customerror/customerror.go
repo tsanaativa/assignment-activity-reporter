8d981d909582bd887b5cc7e53d9ec0a715013f4f
tsanaativa <tsanaativa.vinnera@gmail.com>
2024-01-23 16:26:38 +0700

    feat: create custom errors

---
+++ customerror/customerror.go
@@ -0,0 +1,28 @@
+package customerror
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrInvalidMenu    = errors.New("invalid menu")
+	ErrInvalidInput   = errors.New("invalid input")
+	ErrInvalidKeyword = errors.New("invalid keyword")
+
+	ErrAlreadyFollowed  = errors.New("you already followed the user")
+	ErrAlreadyLiked     = errors.New("you already liked the photo")
+	ErrFollowThemselves = errors.New("a user cannot follow themselves")
+)
+
+func ErrUnknownUser(username string) error {
+	return fmt.Errorf("unknown user %s", username)
+}
+
+func ErrPhotoDoesntExist(username string) error {
+	return fmt.Errorf("%s doesn't have a photo", username)
+}
+
+func ErrUnableToLike(username string) error {
+	return fmt.Errorf("unable to like %s's photo", username)
+}
